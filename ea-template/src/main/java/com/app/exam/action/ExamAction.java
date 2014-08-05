dd671b7ff14fb1fa8e81afef8024500847eb3cd6
Verten <342054368@qq.com>
2014-08-05 14:53:54 +0900

    修改邮件发送格式

--- ea-template/src/main/java/com/app/exam/action/ExamAction.java
+++ ea-template/src/main/java/com/app/exam/action/ExamAction.java
@@ -503,7 +503,7 @@ public class ExamAction extends BaseProcessAction {
 							+ paper.getName()
 							+ "</font><br/>The exam enter time starts at <font color='red'>"
 							+ starttime
-							+ "</font><br/>Please attend the exam on time!" + "<br/><font color='red'>URL:<a href='"+url+"'>"+url+"</a></font>";
+							+ "</font>" + "<br/><font color='red'>URL:<a href='"+url+"'>"+url+"</a></font><br/><br/>Please attend the exam on time!";
 					Map<String,String> data = sendStartEmail(assignee, content);
 					Set<String> mails = data.keySet();
 					for (String mail : mails) {
